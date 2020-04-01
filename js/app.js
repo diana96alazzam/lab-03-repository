a6af162f43aed7b1d2003e83d0a5fd0ef04c8357
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-04-01 15:05:09 +0300

    minimized the code according to code review

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 'use strict';
-let keywordsP1 = [];
-let keywordsP2 = [];
-let p1animals = [];
-let p2animals = [];
+
+let keywords = [];
+
+let allAnimals =[];
 
 
 $(document).ready(function () {
@@ -15,141 +15,67 @@ $(document).ready(function () {
     this.horns = animal.horns;
   }
 
-
-  Animal.prototype.renderP1 = function () {
-
-    let $animalTemplate = $('#photo-template').html();
-    var rendered = Mustache.render($animalTemplate, this);
-    $('#page1div').append(rendered);
-  };
-
-  Animal.prototype.renderP2 = function () {
+  Animal.prototype.render = function () {
 
     let $animalTemplate = $('#photo-template').html();
     var rendered = Mustache.render($animalTemplate, this);
-    $('#page2div').append(rendered);
+    $('main').append(rendered);
 
   };
-  const readJason1 = () => {
 
-    $.ajax('data/page-1.json', { method: 'GET', dataType: 'JSON' }).then(data => {
+  Animal.prototype.filters = function() {
+    console.log(this.keyword);
+    if (!keywords.includes(this.keyword)) {
+      keywords.push(this.keyword);
+      $('select').append($('<option></option>').html(this.keyword).attr('id', this.keyword).attr('class', 'dropDown'));
+    }
+  };
 
-      data.sort((a,b) => a.horns - b.horns);
-      console.log(data);
+
+  const readJson = (pageNum) => {
+
+    $.ajax(`data/page-${pageNum}.json`, { method: 'GET', dataType: 'JSON' }).then(data => {
 
       data.forEach(newEle => {
         let newAnimal = new Animal(newEle);
-        newAnimal.renderP1();
+        newAnimal.render();
+        newAnimal.filters();
+        allAnimals.push(newAnimal);
 
-        p1animals.push(newAnimal);
-        for (let i = 0; i<p1animals.length; i++){
-          $(`section:nth-of-type(${i})`).attr('class', p1animals[i].keyword).attr('id', p1animals[i].title);
+        for (let i = 0; i<allAnimals.length; i++){
+          $(`section:nth-of-type(${i})`).attr('class', allAnimals[i].keyword).attr('id', allAnimals[i].title);
 
         }
 
-        if (!(keywordsP1.includes(newEle.keyword))) {
-          keywordsP1.push(newAnimal.keyword);
-        }
       });
 
-      keywordsP1.forEach(function (filterEl) {
-        $('select').append($('<option></option>').html(filterEl).attr('id', filterEl).attr('class', 'dropDown'));
-      });
     });
 
   };
 
-  const readJason2 = () => {
-    $.ajax('data/page-2.json', { method: 'GET', dataType: 'JSON' }).then(data => {
-      data.forEach(newEle => {
-        let newAnimalP2 = new Animal(newEle);
-        newAnimalP2.renderP2();
-
-        p2animals.push(newAnimalP2);
-
-        for (let i = 0; i<p2animals.length; i++){
-          $(`section:nth-of-type(${i})`).attr('class', p2animals[i].keyword).attr('id', p2animals[i].title);
-        }
-
-        if (!(keywordsP2.includes(newEle.keyword))) {
-          keywordsP2.push(newAnimalP2.keyword);
-        }
-
-      });
-
-      keywordsP2.forEach(function (filterEl) {
-        $('select').append($('<option></option>').html(filterEl).attr('id', filterEl).attr('class', 'dropDown'));
-      });
-    });
-
-  };
-
-  $('main button:first-of-type').on('click', function () {
-    $('#page1div section').show();
-    $('#page2div section').hide();
-
-    $('select').find('option').not('option:nth-of-type(1)').remove();
-
-    keywordsP1.forEach(function (filterEl) {
-      $('select').append($('<option></option>').html(filterEl).attr('id', filterEl).attr('class', 'dropDown'));
-    });
-
-    $('select').on('change', function () {
-
-      $('#page1div section').show();
-      $('#page2div section').hide();
-
-      var selectedVal = $(this).find(':selected').val();
-      // alert(selectedVal);
-      $(`#page1div section:not(.${selectedVal})`).hide();
-
-    }
-    );
-
-  });
-
-
-  $('main button:nth-of-type(2)').on('click', function () {
-    $('#page2div section').show();
-    $('#page1div section').hide();
-
-    $('select').find('option').not('option:nth-of-type(1)').remove();
-
-
-    keywordsP2.forEach(function (filterEl) {
-      $('select').append($('<option></option>').html(filterEl).attr('id', filterEl).attr('class', 'dropDown'));
-    });
-
-    $('select').on('change', function () {
-
-      $('#page2div section').show();
-      $('#page1div section').hide();
-
-      var selectedVal = $(this).find(':selected').val();
-      // alert(selectedVal);
-      $(`#page2div section:not(.${selectedVal})`).hide();
-
-    }
-    );
-
-  });
-
+  readJson(1);
 
   $('select').on('change', function () {
 
-    $('#page1div section').show();
-    $('#page2div section').show();
+    $('section').hide();
+
+    let selectedVal = $(this).find(':selected').val();
+    $(`.${selectedVal}`).show();
+  });
 
 
-    var selectedVal = $(this).find(':selected').val();
-    // alert(selectedVal);
-    $(`#page1div section:not(.${selectedVal})`).hide();
-    $(`#page2div section:not(.${selectedVal})`).hide();
 
-  }
-  );
+  $('button:nth-of-type(1)').on('click', function () {
 
+    $('section').hide();
+    readJson(1);
+  });
 
-  readJason1();
-  readJason2();
+  $('button:nth-of-type(2)').on('click', function () {
+
+    $('section').hide();
+    readJson(2);
+
+  });
+
 });
